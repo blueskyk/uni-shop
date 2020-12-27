ca4106426c9d11db450c69499a5e88b63b0184c2
blueskyk <1419293032@qq.com>
2020-12-27 16:28:49 +0800

    封装自己的请求方法挂载到实例上

---
+++ project-shop/utils/request.js
@@ -0,0 +1,16 @@
+export default (params) => {
+	return new Promise((resolve,reject) => {
+		uni.request({
+			...params,
+			success: (res) => {
+				resolve(res)
+			},
+			error: (err) => {
+				reject(err)
+			},
+			complete() {
+				
+			}
+		})
+	}) 
+}
